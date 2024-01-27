2f57bac51ab3a26573f776971125788c77fd3704
大橋　司 <TSUKASA@oohashishi-no-MacBook-Pro.local>
2024-01-27 16:23:34 +0900

    third  commit

--- app/models/scheduledate.rb
+++ app/models/scheduledate.rb
@@ -1,5 +1,5 @@
 class Scheduledate < ApplicationRecord
-    validates :title, presence: true,length: { in: 1..20 }
+    validates :title, presence: true,length: { in: 1..20 } 
     validates :start, presence: true
     validates :end_date, presence: true
     validates :memo, length: { in: 0..500 }
@@ -8,6 +8,6 @@ class Scheduledate < ApplicationRecord
     private
     
 def start_end
-    errors.add(:end_date, '表示させたいエラーメッセージ') if end_date.nil? || end_date < start
+    errors.add(:end_date, 'は開始日よりあとの日付にしてください') if end_date.nil? || end_date < start
 end
 end
